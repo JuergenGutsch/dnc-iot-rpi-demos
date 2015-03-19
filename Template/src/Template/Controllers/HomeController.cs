b22994cfb9b4227b7d99f4930289e44b2d803d15
JuergenGutsch <juergen@gutsch-online.de>
2015-03-19 07:16:05 +0100

    add template project

---
+++ Template/src/Template/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Mvc;
+using MvcSample.Web.Models;
+using Raspberry;
+
+namespace MvcSample.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(User());
+        }
+
+        public User User()
+        {
+            User user = new User()
+            {
+                Name = "Jürgen Gutsch",
+                Address = "78267 Aach",
+		Board = Board.Current
+            };
+
+            return user;
+        }
+    }
+}
---
+++ Template/src/Template/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Raspberry;
+
+namespace MvcSample.Web.Models
+{
+    public class User
+    {
+        [Required]
+        [MinLength(4)]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+	public Board Board { get; set; }
+    }
+}
