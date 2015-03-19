fb98b598a6720a52f3379aadfb550a61a99b3c16
JuergenGutsch <juergen@gutsch-online.de>
2015-03-19 07:14:21 +0100

    add from template"

---
+++ ReadSensors/src/ReadSensors/Startup.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.DependencyInjection;
+
+namespace HelloMvc
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseErrorPage();
+
+            app.UseServices(services =>
+            {
+                services.AddMvc();
+            });
+
+            app.UseMvc(routes =>
+	    {
+		routes.MapRoute(
+        	name: "default",
+        	template: "{controller}/{action}/{id?}",
+        	defaults: new { controller = "Home", action = "Index" });
+	    });
+
+            app.UseWelcomePage();
+        }       
+    }
+}
